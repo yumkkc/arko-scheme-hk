8e4c26376f56c61d1cac56caf01e55975a2820f4
yum <46126124+yumkkc@users.noreply.github.com>
2026-09-08 14:55:56 +0530

    implement repl

--- app/Main.hs
+++ app/Main.hs
@@ -2,9 +2,36 @@ module Main where
 
 import Eval
 import Parser
+import System.IO
+import System.Environment (getArgs)
+
+flushStr :: String -> IO()
+flushStr str = putStr str >> hFlush stdout
+
+readPrompt :: String -> IO String
+readPrompt prompt = flushStr prompt >> getLine
+
+evalString :: String -> IO String
+evalString expr = return $ extractValue $ trapError $ show <$> (readExpr expr >>= eval)
+
+evalAndPrint :: String -> IO()
+evalAndPrint expr = evalString expr >>= putStrLn
+
+until_ :: Monad m => (a -> Bool) -> m a -> (a -> m ()) -> m ()
+until_ pred prompt action = do
+  result <- prompt
+  if pred result
+    then return ()
+    else action result >> until_ pred prompt action
+
+runRepl :: IO()
+runRepl = until_ (== "quit") (readPrompt "Bebba>> ") evalAndPrint
 
 main :: IO ()
-main = do
-  args <- getLine
-  evaled <- return $ show <$> (readExpr args >>= eval)
-  putStrLn $ extractValue $ trapError evaled
+main = do args <- getArgs
+          case length args of
+            0 -> runRepl
+            1 -> evalAndPrint $ head args
+            _ -> putStrLn "Program only take 0 or 1 arguments"
+            
+           
